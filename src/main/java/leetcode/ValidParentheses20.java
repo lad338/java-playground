e709194e2b52ad3bf616e63ccf72ad4d575dee97
Dicky Li <dickyli338@gmail.com>
2022-07-22 16:10:37 +0800

    add leetcode 20 and 1909

---
+++ src/main/java/leetcode/ValidParentheses20.java
@@ -0,0 +1,43 @@
+package leetcode;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class ValidParentheses20 {
+    static class Solution {
+        public boolean isValid(String s) {
+            Deque<Character> stack = new ArrayDeque<>();
+            for (int i = 0; i < s.length(); i++) {
+                char current = s.charAt(i);
+                if (current == '(' || current == '{' || current == '[') {
+                    stack.push(current);
+                }
+                if (stack.size() == 0) {
+                    return false;
+                }
+
+                if (current == ')') {
+                    if (!stack.pop().equals('(')) {
+                        return false;
+                    }
+                }
+                if (current == '}') {
+                    if (!stack.pop().equals('{')) {
+                        return false;
+                    }
+                }
+                if (current == ']') {
+                    if (!stack.pop().equals('[')) {
+                        return false;
+                    }
+                }
+            }
+            return stack.size() == 0;
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.isValid("([)]"));
+    }
+}
