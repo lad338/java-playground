77e5d19ea53291238f8030124d194733a177003d
Dicky Li <dickyli338@gmail.com>
2022-07-23 11:41:05 +0800

    add leetcode 6 and 21

---
+++ src/main/java/leetcode/ListNode21.java
@@ -0,0 +1,22 @@
+package leetcode;
+
+public class ListNode21 {
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+}
---
+++ src/main/java/leetcode/MergeSortedList21.java
@@ -0,0 +1,21 @@
+package leetcode;
+
+import leetcode.ListNode21.*;
+
+public class MergeSortedList21 {
+
+    static class Solution {
+        public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+            if (list2 == null) {
+                return list1;
+            }
+            if (list1 == null) {
+                return list2;
+            }
+            if (list1.val <= list2.val) {
+                return new ListNode(list1.val, mergeTwoLists(list1.next, list2));
+            }
+            return new ListNode(list2.val, mergeTwoLists(list1, list2.next));
+        }
+    }
+}
