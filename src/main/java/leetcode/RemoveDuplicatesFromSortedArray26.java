e1f1fd34b2c4484490dca3be61ae29e083cefb36
Dicky Li <dickyli338@gmail.com>
2022-07-28 12:01:10 +0800

    add leetcode 26

---
+++ src/main/java/leetcode/RemoveDuplicatesFromSortedArray26.java
@@ -0,0 +1,19 @@
+package leetcode;
+
+public class RemoveDuplicatesFromSortedArray26 {
+    static class Solution {
+        public int removeDuplicates(int[] nums) {
+            int previous = nums[0];
+            int uniqueIndex = 0;
+            for (int i = 1; i < nums.length; i++) {
+                int current = nums[i];
+                if (previous != current) {
+                    uniqueIndex ++;
+                    nums[uniqueIndex] = current;
+                    previous = current;
+                }
+            }
+            return uniqueIndex + 1;
+        }
+    }
+}
