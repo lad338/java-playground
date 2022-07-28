7dfa2f7c7a534f09ad26945007d6e22d0a3a796f
Dicky Li <dickyli338@gmail.com>
2022-07-28 12:20:45 +0800

    Add leetcode 27

---
+++ src/main/java/leetcode/RemoveElements27.java
@@ -0,0 +1,22 @@
+package leetcode;
+
+public class RemoveElements27 {
+    static class Solution {
+        public int removeElement(int[] nums, int val) {
+            int backIndex = nums.length;
+            for (int i = 0; i < nums.length && backIndex > i; i++) {
+                if (nums[i] == val) {
+                    backIndex--;
+                    int backElement = nums[backIndex];
+                    while(backElement == val && backIndex > i) {
+                        backIndex--;
+                        backElement = nums[backIndex];
+                    }
+                    nums[backIndex] = nums[i];
+                    nums[i] = backElement;
+                }
+            }
+            return backIndex;
+        }
+    }
+}
