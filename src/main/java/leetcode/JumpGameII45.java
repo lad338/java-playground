6c6767d3cfd0892e86ec87d3bd4a91981e830331
Dicky Li <dickyli338@gmail.com>
2022-07-28 16:51:25 +0800

    add leetcode 7, 8 and 45

---
+++ src/main/java/leetcode/JumpGameII45.java
@@ -0,0 +1,19 @@
+package leetcode;
+
+public class JumpGameII45 {
+    static class Solution {
+        public int jump(int[] nums) {
+            int result = 0;
+            int maxDist = 0;
+            int current = 0;
+            for (int i = 0; i < nums.length - 1; i++ ) {
+                maxDist = Math.max(maxDist, i + nums[i]);
+                if (i == current) {
+                    result++;
+                    current = maxDist;
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ src/main/java/leetcode/ReverseInteger7.java
@@ -0,0 +1,55 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ReverseInteger7 {
+    static class Solution {
+        public int reverse(int x) {
+            if (Integer.MIN_VALUE == x) {
+                return 0;
+            }
+            boolean isDone = false;
+            List<Integer> digits = new ArrayList<>();
+            boolean isPositive = x >= 0;
+            if (!isPositive) {
+                x = -x;
+            }
+
+            int length = (int) (Math.log10(x) + 1);
+            for (int i = 0; i < length; i++) {
+                int digit = x / ((int) Math.pow(10, i)) % 10;
+                digits.add(digit);
+            }
+
+            int result = 0;
+            boolean hasNonZero = false;
+
+            try {
+                for (int i = 0; i < digits.size(); i++) {
+                    int current = digits.get(i);
+                    if (current != 0) {
+                        result = Math.addExact(
+                                result,
+                                (isPositive? 1 : -1) * Math.multiplyExact(
+                                        current,
+                                        (int) (Math.pow(10, digits.size() - i - 1))
+                                )
+                        );
+                        if (i == 0) {
+                            if (isPositive && result < 0) {
+                                return 0;
+                            } else if (!isPositive && result >0) {
+                                return 0;
+                            }
+                        }
+                    }
+                }
+            } catch (Exception e) {
+                return 0;
+            }
+
+            return result;
+        }
+    }
+}
