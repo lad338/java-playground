9809d413e18739bd6787ec7d9baa412540e841a1
Dicky Li <dickyli338@gmail.com>
2022-07-29 23:35:34 +0800

    add leetcode 35

---
+++ src/main/java/leetcode/SearchInsertPosition35.java
@@ -0,0 +1,35 @@
+package leetcode;
+import java.util.Arrays;
+
+public class SearchInsertPosition35 {
+    static class Solution {
+        public int searchInsert(int[] nums, int target) {
+            int length = nums.length;
+            if (length == 0) {
+                return 0;
+            }
+            if (length == 1) {
+                if (nums[0] >= target) {
+                    return 0;
+                }
+                return 1;
+            }
+
+            int middlePoint = length / 2;
+            if (nums[middlePoint] == target) {
+                return middlePoint;
+            }
+            if (nums[middlePoint] > target) {
+                return searchInsert(Arrays.copyOfRange(nums, 0 , middlePoint), target);
+            }
+            if (nums[middlePoint] < target) {
+                if (middlePoint + 1 > length) {
+                    return middlePoint + length + 1;
+                } else {
+                    return middlePoint + searchInsert(Arrays.copyOfRange(nums, middlePoint + 1 , length), target) + 1;
+                }
+            }
+            throw null;
+        }
+    }
+}
