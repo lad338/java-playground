e0f6daf033741151b4aae4dd8363ef0053d47aa9
Dicky Li <dickyli338@gmail.com>
2022-08-01 01:33:31 +0800

    add leetcode 58

---
+++ src/main/java/leetcode/LengthOfLastWord58.java
@@ -0,0 +1,26 @@
+package leetcode;
+
+public class LengthOfLastWord58 {
+    static class Solution {
+        public int lengthOfLastWord(String s) {
+            boolean isWord = Character.isAlphabetic(s.charAt(0));
+            int result = isWord? 1 : 0;
+            for (int i = 1; i < s.length(); i++) {
+                char current = s.charAt(i);
+                if (Character.isAlphabetic(current)) {
+                    if (isWord) {
+                        result ++;
+                    } else {
+                        result = 1;
+                        isWord = true;
+                    }
+                } else {
+                    if (isWord) {
+                        isWord = false;
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}
