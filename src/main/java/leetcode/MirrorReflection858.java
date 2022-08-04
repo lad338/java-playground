cdc8370bcc1a3ea58302b817d0a5b80e2fdf5be1
Dicky Li <dickyli338@gmail.com>
2022-08-04 11:53:55 +0800

    add leetcode 12, 858

---
+++ src/main/java/leetcode/MirrorReflection858.java
@@ -0,0 +1,21 @@
+package leetcode;
+
+public class MirrorReflection858 {
+
+  static class Solution {
+
+    public int mirrorReflection(int p, int q) {
+      while (p % 2 == 0 && q % 2 == 0) {
+        p /= 2;
+        q /= 2;
+      }
+
+      if (p % 2 == 0 && q % 2 == 1) {
+        return 2;
+      } else if (p % 2 == 1 && q % 2 == 1) {
+        return 1;
+      }
+      return 0;
+    }
+  }
+}
