530fce723588570368fc2a1e15972d8413d26e95
Dicky Li <dickyli338@gmail.com>
2022-09-08 14:42:43 +0800

    add more memory efficient solution for leetcode 94

--- src/main/java/leetcode/BinaryTreeInorderTraversal94.java
+++ src/main/java/leetcode/BinaryTreeInorderTraversal94.java
@@ -1,6 +1,7 @@
 package leetcode;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class BinaryTreeInorderTraversal94 {
@@ -20,6 +21,39 @@ public class BinaryTreeInorderTraversal94 {
 
             result.add(root.val);
             result.addAll(rightList);
+            return result;
+        }
+    }
+
+    class Solution2 {
+
+        public List<Integer> inorderTraversal(TreeNode root) {
+            if (root == null) {
+                return Collections.emptyList();
+            }
+            List<Integer> left = root.left == null
+                ? null
+                : inorderTraversal(root.left);
+            List<Integer> right = root.right == null
+                ? null
+                : inorderTraversal(root.right);
+
+            List<Integer> result;
+
+            if (left != null) {
+                result = left;
+                result.add(root.val);
+            } else {
+                result =
+                    new ArrayList<>() {
+                        {
+                            add(root.val);
+                        }
+                    };
+            }
+            if (right != null) {
+                result.addAll(right);
+            }
             return result;
         }
     }
