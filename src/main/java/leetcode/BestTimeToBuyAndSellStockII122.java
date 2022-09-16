a6df79e89e42d049dc74311859da3bdc684846da
Dicky Li <dickyli338@gmail.com>
2022-09-16 17:24:21 +0800

    add leetcode 122

---
+++ src/main/java/leetcode/BestTimeToBuyAndSellStockII122.java
@@ -0,0 +1,19 @@
+package leetcode;
+
+public class BestTimeToBuyAndSellStockII122 {
+
+    class Solution {
+
+        public int maxProfit(int[] prices) {
+            int[] buy = new int[prices.length];
+            int[] sell = new int[prices.length];
+            sell[0] = 0;
+            buy[0] = -prices[0];
+            for (int i = 1; i < prices.length; i++) {
+                sell[i] = Math.max(sell[i - 1], buy[i - 1] + prices[i]);
+                buy[i] = Math.max(buy[i - 1], sell[i - 1] - prices[i]);
+            }
+            return sell[prices.length - 1];
+        }
+    }
+}
