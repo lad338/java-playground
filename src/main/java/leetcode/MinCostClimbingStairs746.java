048a2092a6cdcf42fb249cb1d9450fcb5ead1152
Dicky Li <dickyli338@gmail.com>
2022-09-19 17:40:43 +0800

    add leetcode 746

---
+++ src/main/java/leetcode/MinCostClimbingStairs746.java
@@ -0,0 +1,23 @@
+package leetcode;
+
+public class MinCostClimbingStairs746 {
+
+    class Solution {
+
+        public int minCostClimbingStairs(int[] cost) {
+            int goal = cost.length;
+
+            int[] dp = new int[goal];
+
+            for (int i = 0; i < goal; i++) {
+                if (i < 2) {
+                    dp[i] = cost[goal - 1 - i];
+                } else {
+                    dp[i] = cost[goal - 1 - i] + Math.min(dp[i - 1], dp[i - 2]);
+                }
+            }
+
+            return Math.min(dp[goal - 1], dp[goal - 2]);
+        }
+    }
+}
