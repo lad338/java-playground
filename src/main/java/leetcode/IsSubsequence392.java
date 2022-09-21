cdcf85de4c488ab9432d2eda975ddc7b03f08ea5
Dicky Li <dickyli338@gmail.com>
2022-09-21 16:31:56 +0800

    add solution to followup question for leetcode 392

--- src/main/java/leetcode/IsSubsequence392.java
+++ src/main/java/leetcode/IsSubsequence392.java
@@ -1,5 +1,7 @@
 package leetcode;
 
+import java.util.*;
+
 public class IsSubsequence392 {
 
     class Solution {
@@ -40,6 +42,53 @@ public class IsSubsequence392 {
                 return false;
             }
             return isSubsequence(s.substring(1), t.substring(index + 1));
+        }
+    }
+
+    class FollowupSolution {
+
+        Map<Character, List<Integer>> map = new HashMap<>();
+
+        public boolean isSubsequence(String s, String t) {
+            constructMap(t);
+            return binarySearchMap(s);
+        }
+
+        private void constructMap(String t) {
+            for (int i = 0; i < t.length(); i++) {
+                List<Integer> cList = map.getOrDefault(
+                    t.charAt(i),
+                    new ArrayList<>()
+                );
+                cList.add(i);
+                map.put(t.charAt(i), cList);
+            }
+        }
+
+        private boolean binarySearchMap(String s) {
+            int index = 0;
+            for (char c : s.toCharArray()) {
+                List<Integer> cList = map.getOrDefault(
+                    c,
+                    Collections.emptyList()
+                );
+                if (cList.size() == 0) {
+                    return false;
+                }
+                int binarySearch = Collections.binarySearch(cList, index);
+
+                if (binarySearch < 0) {
+                    binarySearch = -binarySearch - 1;
+                }
+
+                if (binarySearch == cList.size()) {
+                    return false;
+                }
+
+                index = cList.get(binarySearch) + 1;
+            }
+
+            return true;
         }
     }
 }
