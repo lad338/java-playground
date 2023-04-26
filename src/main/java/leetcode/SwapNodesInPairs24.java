25b621842acb991d0cc4c871815adca86f0999b3
Dicky Li <dickyli338@gmail.com>
2023-04-26 14:10:52 -0400

    add leetcode 24

---
+++ src/main/java/leetcode/SwapNodesInPairs24.java
@@ -0,0 +1,49 @@
+package leetcode;
+
+import leetcode.ListNode21.*;
+
+public class SwapNodesInPairs24 {
+
+    class Solution {
+
+        public ListNode swapPairs(ListNode head) {
+            if (head == null) {
+                return null;
+            }
+
+            if (head.next == null) {
+                return head;
+            }
+
+            ListNode root = head.next;
+
+            ListNode previous = null;
+            ListNode front = head.next;
+            ListNode back = head;
+
+            while (front != null) {
+                System.out.println(back.val);
+                System.out.println(front.val);
+
+                back.next = front.next;
+                front.next = back;
+                if (previous != null) {
+                    previous.next = front;
+                }
+                previous = back;
+
+                back = back.next;
+                if (back == null) {
+                    break;
+                }
+                if (back.next != null) {
+                    front = back.next;
+                } else {
+                    break;
+                }
+            }
+
+            return root;
+        }
+    }
+}
