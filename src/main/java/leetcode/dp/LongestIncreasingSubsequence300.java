d25c3f090bfa15843f0125f4bb97e004c74bd2b3
Dicky Li <dickyli338@gmail.com>
2023-10-03 19:40:32 -0400

    add dp solution for lc 300

--- src/main/java/leetcode/dp/LongestIncreasingSubsequence300.java
+++ src/main/java/leetcode/dp/LongestIncreasingSubsequence300.java
@@ -43,4 +43,23 @@ public class LongestIncreasingSubsequence300 {
             }
         }
     }
+
+    class DPSolution {
+
+        public int lengthOfLIS(int[] nums) {
+            int[] dp = new int[nums.length];
+
+            Arrays.fill(dp, 1);
+
+            for (int i = nums.length - 1; i >= 0; i--) {
+                for (int j = i + 1; j < nums.length; j++) {
+                    if (nums[j] > nums[i]) {
+                        dp[i] = Math.max(dp[i], 1 + dp[j]);
+                    }
+                }
+            }
+
+            return Arrays.stream(dp).max().getAsInt();
+        }
+    }
 }
