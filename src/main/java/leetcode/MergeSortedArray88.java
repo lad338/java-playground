f793675d72956d744ac311d377bf95a7bcf77e7d
Dicky Li <dickyli338@gmail.com>
2023-10-28 13:33:18 -0400

    add leetcode 88

---
+++ src/main/java/leetcode/MergeSortedArray88.java
@@ -0,0 +1,33 @@
+package leetcode;
+
+public class MergeSortedArray88 {
+
+    class Solution {
+
+        public void merge(int[] nums1, int m, int[] nums2, int n) {
+            int index = m + n - 1;
+            m = m - 1;
+            n = n - 1;
+            while (m >= 0 && n >= 0) {
+                if (nums1[m] > nums2[n]) {
+                    nums1[index] = nums1[m];
+                    m--;
+                } else {
+                    nums1[index] = nums2[n];
+                    n--;
+                }
+                index--;
+            }
+            while (m >= 0) {
+                nums1[index] = nums1[m];
+                m--;
+                index--;
+            }
+            while (n >= 0) {
+                nums1[index] = nums2[n];
+                n--;
+                index--;
+            }
+        }
+    }
+}
