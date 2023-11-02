fe8367fd53cb392eaaa4033d22c4167f905f21df
Dicky Li <dickyli338@gmail.com>
2023-11-02 18:18:12 -0400

    add neetcode solution for lc 905

--- src/main/java/leetcode/SortArrayByParity905.java
+++ src/main/java/leetcode/SortArrayByParity905.java
@@ -27,4 +27,20 @@ public class SortArrayByParity905 {
             return nums;
         }
     }
+
+    class NeetcodeSolution {
+
+        public int[] sortArrayByParity(int[] nums) {
+            int l = 0;
+            for (int r = 0; r < nums.length; r++) {
+                if (nums[r] % 2 == 0) {
+                    int tmp = nums[l];
+                    nums[l] = nums[r];
+                    nums[r] = tmp;
+                    l++;
+                }
+            }
+            return nums;
+        }
+    }
 }
