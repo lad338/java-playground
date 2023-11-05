5240cdf2e133792d2473429bc6f4e38c27dee9ee
Dicky Li <dickyli338@gmail.com>
2023-11-05 00:33:26 -0400

    add iterative solution for A interview question

--- src/main/java/interview/MinimumTimeToCompleteAllJobs.java
+++ src/main/java/interview/MinimumTimeToCompleteAllJobs.java
@@ -7,10 +7,12 @@ public class MinimumTimeToCompleteAllJobs {
     public static class Job {
 
         int time;
+        int index;
         List<Integer> dependencies;
 
-        public Job(int time, List<Integer> dependencies) {
+        public Job(int time, int index, List<Integer> dependencies) {
             this.time = time;
+            this.index = index;
             this.dependencies = dependencies;
         }
     }
@@ -52,21 +54,50 @@ public class MinimumTimeToCompleteAllJobs {
         return result;
     }
 
+    public int minimumTimeIterative(List<Job> jobs) {
+        int[] cache = new int[jobs.size()];
+
+        Queue<Job> queue = new ArrayDeque<>(jobs);
+
+        while (!queue.isEmpty()) {
+            Job job = queue.poll();
+            if (job.dependencies.size() == 0) {
+                cache[job.index] = job.time;
+                continue;
+            }
+
+            int max = 0;
+            boolean notFound = false;
+            for (int i = 0; i < job.dependencies.size() && !notFound; i++) {
+                if (cache[job.dependencies.get(i)] != 0) {
+                    max = Math.max(max, cache[job.dependencies.get(i)]);
+                } else {
+                    notFound = true;
+                }
+            }
+            if (notFound) {
+                queue.add(job);
+                continue;
+            }
+            cache[job.index] = job.time + max;
+        }
+
+        return Arrays.stream(cache).max().orElse(-1);
+    }
+
     public static void main(String[] args) {
         MinimumTimeToCompleteAllJobs solution = new MinimumTimeToCompleteAllJobs();
 
-        System.out.println(
-            solution.minimumTime(
-                List.of(
-                    new Job(5, Collections.emptyList()),
-                    new Job(4, List.of(0)),
-                    new Job(3, List.of(0, 1)),
-                    new Job(1, Collections.emptyList()),
-                    new Job(15, List.of(3, 2))
-                )
-            )
+        List<Job> jobs = List.of(
+            new Job(5, 0, Collections.emptyList()),
+            new Job(4, 1, List.of(0)),
+            new Job(3, 2, List.of(0, 1)),
+            new Job(1, 3, Collections.emptyList()),
+            new Job(15, 4, List.of(3, 2))
         );
 
-        System.out.println(solution.cache);
+        System.out.println(solution.minimumTime(jobs));
+
+        System.out.println(solution.minimumTimeIterative(jobs));
     }
 }
