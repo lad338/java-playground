ffcccfd222bd41b7978dff4ce10a835fba4ab50d
Dicky Li <dickyli338@gmail.com>
2023-11-17 11:56:52 -0500

    add lc 371

---
+++ src/main/java/leetcode/bit/Sum2Integers371.java
@@ -0,0 +1,15 @@
+package leetcode.bit;
+
+public class Sum2Integers371 {
+
+    class Solution {
+
+        public int getSum(int a, int b) {
+            if (b == 0) {
+                return a;
+            }
+            int carry = (a & b) << 1;
+            return getSum(a ^ b, carry);
+        }
+    }
+}
