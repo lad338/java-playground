e60bf93865f97f9a42a97bfe5fe7488fa4884083
Dicky Li <dickyli338@gmail.com>
2023-11-29 13:22:11 -0500

    add greedy solution to jump game 55

--- src/main/java/leetcode/dp/JumpGame55.java
+++ src/main/java/leetcode/dp/JumpGame55.java
@@ -22,4 +22,37 @@ public class JumpGame55 {
             return reversedCheck[0];
         }
     }
+
+    class BackwardsGreedySolution {
+
+        public boolean canJump(int[] nums) {
+            if (nums.length == 1) {
+                return true;
+            }
+
+            int goal = nums.length - 1;
+
+            for (int i = nums.length - 2; i >= 0; i--) {
+                if (i + nums[i] >= goal) {
+                    goal = i;
+                }
+            }
+
+            return goal == 0;
+        }
+    }
+
+    //
+    class ForwardGreedySolution {
+
+        public boolean canJump(int[] nums) {
+            int maxDistance = 0;
+            for (int i = 0; i < nums.length - 1; i++) {
+                if (i <= maxDistance) {
+                    maxDistance = Math.max(maxDistance, i + nums[i]);
+                }
+            }
+            return maxDistance >= nums.length - 1;
+        }
+    }
 }
