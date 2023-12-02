f11e848e1065095e8cac91024e3f91b2cd6f411a
Dicky Li <dickyli338@gmail.com>
2023-12-02 12:44:33 -0500

    add recursive solution for lc230

--- src/main/java/leetcode/KthSmallestElementBST230.java
+++ src/main/java/leetcode/KthSmallestElementBST230.java
@@ -41,4 +41,43 @@ public class KthSmallestElementBST230 {
             }
         }
     }
+
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     *     int val;
+     *     TreeNode left;
+     *     TreeNode right;
+     *     TreeNode() {}
+     *     TreeNode(int val) { this.val = val; }
+     *     TreeNode(int val, TreeNode left, TreeNode right) {
+     *         this.val = val;
+     *         this.left = left;
+     *         this.right = right;
+     *     }
+     * }
+     */
+    class InorderRecursiveSolution {
+
+        int k;
+        int result;
+
+        public int kthSmallest(TreeNode root, int k) {
+            this.k = k;
+            helper(root);
+            return result;
+        }
+
+        public void helper(TreeNode node) {
+            if (node == null) {
+                return;
+            }
+            helper(node.left);
+            k--;
+            if (k == 0) {
+                result = node.val;
+            }
+            helper(node.right);
+        }
+    }
 }
