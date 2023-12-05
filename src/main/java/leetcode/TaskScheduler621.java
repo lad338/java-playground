b2944d39101480ba8868caf2d89d56f22a1ee412
Dicky Li <dickyli338@gmail.com>
2023-12-05 15:55:49 -0500

    add lc621

---
+++ src/main/java/leetcode/TaskScheduler621.java
@@ -0,0 +1,46 @@
+package leetcode;
+
+import java.util.*;
+
+public class TaskScheduler621 {
+
+    class Solution {
+
+        public int leastInterval(char[] tasks, int n) {
+            int[] countMap = new int[26];
+            for (char c : tasks) {
+                countMap[c - 'A'] += 1;
+            }
+
+            PriorityQueue<int[]> maxHeap = new PriorityQueue<>((a, b) ->
+                b[0] - a[0]
+            );
+
+            for (int count : countMap) {
+                if (count == 0) {
+                    continue;
+                }
+                maxHeap.offer(new int[] { count, 0 });
+            }
+
+            Deque<int[]> queue = new ArrayDeque<>();
+
+            int result = 0;
+            while (!maxHeap.isEmpty() || !queue.isEmpty()) {
+                while (!queue.isEmpty() && queue.peek()[1] < result) {
+                    maxHeap.offer(queue.poll());
+                }
+                if (!maxHeap.isEmpty()) {
+                    int[] first = maxHeap.poll();
+                    if (first[0] > 1) {
+                        first[0] -= 1;
+                        first[1] = result + n;
+                        queue.offer(first);
+                    }
+                }
+                result++;
+            }
+            return result;
+        }
+    }
+}
