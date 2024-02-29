8a8bddb594055816b079d2a66760a5e1754b0da7
Dicky Li <dickyli338@gmail.com>
2024-02-29 15:49:03 -0500

    add solution2 for lc133

--- src/main/java/leetcode/CloneGraph133.java
+++ src/main/java/leetcode/CloneGraph133.java
@@ -29,4 +29,30 @@ public class CloneGraph133 {
             return newNode;
         }
     }
+
+    class NonUniqueValueSolution {
+
+        //value: node
+        Map<Node, Node> map = new HashMap<>();
+
+        public Node cloneGraph(Node node) {
+            if (node == null) {
+                return null;
+            }
+
+            if (map.containsKey(node)) {
+                return map.get(node);
+            }
+
+            Node newNode = new Node(node.val, new ArrayList<>());
+            map.put(node, newNode);
+
+            for (Node nei : node.neighbors) {
+                Node newNei = cloneGraph(nei);
+                newNode.neighbors.add(newNei);
+            }
+
+            return newNode;
+        }
+    }
 }
